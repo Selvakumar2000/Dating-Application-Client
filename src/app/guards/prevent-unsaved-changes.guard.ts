8742d1b02ed40806911c490ac214eb04f91c400a
Selvakumar2000 <skselva312000@gmail.com>
2021-09-17 17:58:15 +0530

    "Dating Application Completed without Publishing"

--- src/app/guards/prevent-unsaved-changes.guard.ts
+++ src/app/guards/prevent-unsaved-changes.guard.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(component: MemberEditComponent): boolean{
-    if(component.editForm.dirty)
+
+    constructor(public confirmService: ConfirmService) {}
+
+
+    canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean
     {
-      return confirm('Are you sure you want to continue? All unsaved changes will be lost');
+      if(component.editForm.dirty)
+      {
+        return this.confirmService.confirm();
+      }
+      return true;
     }
-    return true;
-  }
   
 }
